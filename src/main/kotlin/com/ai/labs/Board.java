85db208a91350bd6f7fed7762738ff3e8851ef87
DK <novayapochta123@gmail.com>
2020-10-10 18:57:23 +0300

    refactored removal of a pill

--- src/main/kotlin/com/ai/labs/Board.java
+++ src/main/kotlin/com/ai/labs/Board.java
@@ -39,7 +39,7 @@ public class Board extends JPanel implements ActionListener {
     private final int PAC_ANIM_DELAY = 2;
     private final int PACMAN_ANIM_COUNT = 4;
     private final int MAX_GHOSTS = 12;
-    private final int PACMAN_SPEED = 6;
+    private final int PACMAN_SPEED = 12;
 
     private int pacAnimCount = PAC_ANIM_DELAY;
     private int pacAnimDir = 1;
@@ -359,16 +359,20 @@ public class Board extends JPanel implements ActionListener {
 
         Stack<Point> localN = new Stack<>();
 
-        if ((screenData[pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE)] & 16) != 0) {                       //TODO Eats TABLETKA
-            screenData[pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE)] = (short) (screenData[pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE)] & 15);
+        int x = pacman_x / BLOCK_SIZE;
+        int y = pacman_y / BLOCK_SIZE;
+
+        int pos = pointToPos(x, y);
+        short ch = screenData[pos];
+
+        if ((ch & 16) != 0) {
+            screenData[pos] = (short) (ch & 15);        //eats a pill
             score++;
             inGame = false;
             visited.clear();
             neighbours = new Stack<>();
         }
 
-        int x = pacman_x / BLOCK_SIZE;
-        int y = pacman_y / BLOCK_SIZE;
 
         int posUp = pointToPos(x, y-1);
         int posDown = pointToPos(x, y+1);
@@ -390,11 +394,9 @@ public class Board extends JPanel implements ActionListener {
         //pop & append to visited
         visited.add(pointToPos(x, y));
 
-
         Point next;
         if(localN.isEmpty()){
             next = neighbours.pop();
-            //visited.add(pointToPos(next.x, next.y)); //TODO Esli ne komentit to ne rabotaet posle teleporta
             pacman_x = next.x * BLOCK_SIZE;
             pacman_y = next.y * BLOCK_SIZE;
         } else {
@@ -431,8 +433,7 @@ public class Board extends JPanel implements ActionListener {
                 break;
         }
 
-        int pos;
-        short ch;
+
 
         if (req_dx == -pacmand_x && req_dy == -pacmand_y) {
             pacmand_x = req_dx;
@@ -442,42 +443,42 @@ public class Board extends JPanel implements ActionListener {
             view_dy = pacmand_y;
         }
 
-        if (pacman_x % BLOCK_SIZE == 0 && pacman_y % BLOCK_SIZE == 0) {
-            pos = pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE);
-            ch = screenData[pos];
-
-
-//            if ((ch & 16) != 0) {                       //removes a pill
-//                screenData[pos] = (short) (ch & 15);
-//                score++;
-//                inGame = false;
-//                visited.clear();
-//                neighbours = new Stack<>();
+//        if (pacman_x % BLOCK_SIZE == 0 && pacman_y % BLOCK_SIZE == 0) {
+//            pos = pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE);
+//            ch = screenData[pos];
+//
+//
+//
+//            //todo remove ??
+//            if (req_dx != 0 || req_dy != 0) {  //if not stands still
+//                if (!(   (req_dx == -1 && req_dy == 0  && (ch & 1) != 0)          //l
+//                        || (req_dx == 1  && req_dy == 0  && (ch & 4) != 0)        //r
+//                        || (req_dx == 0  && req_dy == -1 && (ch & 2) != 0)        //u
+//                        || (req_dx == 0  && req_dy == 1  && (ch & 8) != 0) ) ) {  //d
+//                    pacmand_x = req_dx;
+//                    pacmand_y = req_dy;
+//                    view_dx = pacmand_x;
+//                    view_dy = pacmand_y;
+//                }
 //            }
+//
+//
+//
+//            // Check for standstill
+//            if ((pacmand_x == -1 && pacmand_y == 0 && (ch & 1) != 0)
+//                    || (pacmand_x == 1 && pacmand_y == 0 && (ch & 4) != 0)
+//                    || (pacmand_x == 0 && pacmand_y == -1 && (ch & 2) != 0)
+//                    || (pacmand_x == 0 && pacmand_y == 1 && (ch & 8) != 0)) {
+//                pacmand_x = 0;
+//                pacmand_y = 0;
+//            }
+//        }
 
-            //todo remove ??
-            if (req_dx != 0 || req_dy != 0) {  //if not stands still
-                if (!(   (req_dx == -1 && req_dy == 0  && (ch & 1) != 0)        //l
-                        || (req_dx == 1  && req_dy == 0  && (ch & 4) != 0)        //r
-                        || (req_dx == 0  && req_dy == -1 && (ch & 2) != 0)        //u
-                        || (req_dx == 0  && req_dy == 1  && (ch & 8) != 0) ) ) {  //d
-                    pacmand_x = req_dx;
-                    pacmand_y = req_dy;
-                    view_dx = pacmand_x;
-                    view_dy = pacmand_y;
-                }
-            }
-
-
-
-            // Check for standstill
-            if ((pacmand_x == -1 && pacmand_y == 0 && (ch & 1) != 0)
-                    || (pacmand_x == 1 && pacmand_y == 0 && (ch & 4) != 0)
-                    || (pacmand_x == 0 && pacmand_y == -1 && (ch & 2) != 0)
-                    || (pacmand_x == 0 && pacmand_y == 1 && (ch & 8) != 0)) {
-                pacmand_x = 0;
-                pacmand_y = 0;
-            }
+        if (pacman_x % BLOCK_SIZE == 0 && pacman_y % BLOCK_SIZE == 0) {
+            pacmand_x = req_dx;
+            pacmand_y = req_dy;
+            view_dx = pacmand_x;
+            view_dy = pacmand_y;
         }
 
         pacman_x = pacman_x + PACMAN_SPEED * pacmand_x;
