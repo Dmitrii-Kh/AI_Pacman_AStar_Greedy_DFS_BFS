f3e5dcdb8153721694b1766d53ff14b1c6008fb7
DK <novayapochta123@gmail.com>
2020-10-11 19:09:03 +0300

    final route

--- src/main/kotlin/com/ai/labs/Point.java
+++ src/main/kotlin/com/ai/labs/Point.java
@@ -4,12 +4,24 @@ public class Point {
     int x, y;
     boolean hasFork;
     int numOfNeighbours;
+    int prevx, prevy;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
         this.hasFork = false;
         this.numOfNeighbours = 0;
+        this.prevx = 0;
+        this.prevy = 0;
+    }
+
+    public Point(int x, int y, int prevx, int prevy) {
+        this.x = x;
+        this.y = y;
+        this.hasFork = false;
+        this.numOfNeighbours = 0;
+        this.prevx = prevx;
+        this.prevy = prevy;
     }
 
     public Point(int x, int y, boolean hasFork, int numOfNeighbours) {
